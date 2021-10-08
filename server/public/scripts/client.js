86adbe243066b06508be421b208fcc2865a6d9cc
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-10-08 16:54:01 -0500

    submits calculation to server - GET /calculate

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,6 +10,7 @@ function onReady() {
 
   // add event listeners
   $(`.mathSymbolButton`).on(`click`, changeSymbol);
+  $(`#equalsButton`).on(`click`, submitCalc);
 }
 
 //
@@ -17,4 +18,20 @@ function changeSymbol() {
   // the text of this button is the math operator needed
   mathSymbol = $(this).text();
   console.log('mathSymbol is', mathSymbol);
+}
+
+function submitCalc() {
+  $.ajax({
+    method: 'POST',
+    url: '/calculate',
+    data: {
+      firstNumber: $(`#firstNumberInput`).val(),
+      secondNumber: $(`#secondNumberInput`).val(),
+      mathSymbol,
+    },
+  }).then(updateDOM);
+}
+
+function updateDOM(res) {
+  console.log(`in updateDOM()`);
 }
