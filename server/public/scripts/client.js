774c01b839cfbdc7b57a4fc300567109deb40ca1
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-10-10 14:35:15 -0500

    adds validation for no operators submitted

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -128,8 +128,15 @@ function validInput(formula) {
     );
     return false;
   }
+
+  let containsOperators = false;
   // test whether any two operators are directly next to each other
   for (let i = 0; i < formula.length - 1; i++) {
+    // at the same time, check whether there are any operators listed at all
+    // if no operators are listed, no calc can be performed
+    if (`+-*/`.includes(formula[i])) {
+      containsOperators = true;
+    }
     if (`+-*/`.includes(formula[i]) && `+-*/`.includes(formula[i + 1])) {
       // the operators are directly next to each other!
       console.log(`the operators are next to each other`);
@@ -138,6 +145,10 @@ function validInput(formula) {
       );
       return false;
     }
+  }
+  // if there are no operators, no calc can be performed
+  if (!containsOperators) {
+    return false;
   }
   return true;
 }
--- server/server.js
+++ server/server.js
@@ -160,13 +160,27 @@ function validInput(formula) {
   if (formula[0] === `/`) {
     return false;
   }
+
+  let containsOperators = false;
   // test whether any two operators are directly next to each other
   for (let i = 0; i < formula.length - 1; i++) {
+    // at the same time, check whether there are any operators listed at all
+    // if no operators are listed, no calc can be performed
+    if (`+-*/`.includes(formula[i])) {
+      containsOperators = true;
+    }
     if (`+-*/`.includes(formula[i]) && `+-*/`.includes(formula[i + 1])) {
       // the operators are directly next to each other!
       console.log(`the operators are next to each other`);
       return false;
     }
+  }
+  // if there are no operators, no calc can be performed
+  if (!containsOperators) {
+    return false;
+  }
+  if (!containsOperators) {
+    return false;
   }
   return true;
 }
